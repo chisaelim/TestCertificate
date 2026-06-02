3270b4c64a63bffd0f14440d2793f00729e119d9
chisaelim <chisae0123@gmail.com>
2026-06-03 00:07:35 +0700

    trust proxy in middleware

--- _backend-app/bootstrap/app.php
+++ _backend-app/bootstrap/app.php
@@ -12,6 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*', headers: Illuminate\Http\Request::HEADER_X_FORWARDED_FOR
+            | Illuminate\Http\Request::HEADER_X_FORWARDED_HOST
+            | Illuminate\Http\Request::HEADER_X_FORWARDED_PORT
+            | Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO);
+
         $middleware->alias([
             '_ADMINISTRATOR_' => App\Http\Middleware\AdministratorMiddleware::class,
             '_DOCUMENT_CONTROLLER_' => App\Http\Middleware\DocumentControllerMiddleware::class,
